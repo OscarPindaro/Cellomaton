13b28297cd2734b6f527f2989f67525d9c44e3c6
Oscar Pindaro <oscar.pindaro@gmail.com>
2024-04-03 20:23:11 +0200

    created some movements so that i can move my cells

---
+++ scenes/movement/follow_movement.gd
@@ -0,0 +1,16 @@
+extends Movement
+class_name FollowMovement
+
+@export var target: Marker2D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super._ready()
+	assert(target != null, "Target of the Follow Movement should not be Null")
+
+func _process(delta):
+	var direction : Vector2 = (target.global_position - body.position).normalized()
+	body.velocity = direction*linear_velocity
+	body.move_and_slide()
+	if look_at:
+		body.look_at(target.position)
---
+++ scenes/movement/movement.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Movement
+
+@export var body: CharacterBody2D = null 
+@export var linear_velocity = 200
+@export var look_at: bool = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if body == null:
+		assert(typeof(get_parent()==CharacterBody2D), "If body is null, the parent should be a CharacterBody so it works")
+		body = get_parent()
+
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
